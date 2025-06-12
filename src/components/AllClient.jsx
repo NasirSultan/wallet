50efc92b803857e6483c15043a987854b288e76f
NasirSultan <rainasirsultan123@gmail.com>
2025-06-12 18:49:06 +0500

    d

--- src/components/AllClient.jsx
+++ src/components/AllClient.jsx
@@ -92,7 +92,7 @@ export default function AllClients() {
                   </div>
 
                   <p className="text-green-300 text-lg">
-                    {t('address')}: <span className="select-text">{client.address}</span>
+                    <span className="select-text">{client.address}</span>
                   </p>
                 </div>
               </div>
@@ -106,7 +106,7 @@ export default function AllClients() {
                 <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M19 14l-7 7m0 0l-7-7m7 7V3" />
                 </svg>
-                {t('view')}
+            
               </button>
 
               <button
@@ -116,7 +116,7 @@ export default function AllClients() {
                 <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M5 10l7-7m0 0l7 7m-7-7v18" />
                 </svg>
-                {t('receive')}
+              
               </button>
             </div>
           </li>
